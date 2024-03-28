8eaa0641c4be8a1053e2bf0fcb72a37a3bab7087
Curvin <roynivruc@gmail.com>
2024-03-28 17:30:18 +0800

    Unfinished

--- Test.java
+++ Test.java
@@ -1,3 +1,18 @@
-public class Test {
-    
+import javax.swing.*;
+
+public class Test extends JFrame {
+    public Test() {
+        JButton button = new JButton("Click me");
+        button.setFocusable(false); // Make the button not focusable
+        add(button);
+
+        setTitle("Focusable Example");
+        setSize(300, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new Test());
+    }
 }
